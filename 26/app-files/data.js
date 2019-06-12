995264c0586f1fe5326c97cff253ca892a4f1496
Martin Virtel <martin.virtel@gmail.com>
2019-06-12 22:42:24 +0200

    initial

---
+++ 26/app-files/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20190608_1553361",
+      "name": "IMG_20190608_155336[1]",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 0.46274475804761117,
+        "pitch": -0.027930202661323733,
+        "fov": 1.0419263748195604
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
---
+++ 27/app-files/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20190608_1610531",
+      "name": "IMG_20190608_161053[1]",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": 1.001044582542555,
+        "pitch": -0.10900040089767771,
+        "fov": 0.9443891853887978
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};

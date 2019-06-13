9d5b313997745dd8a5c98a567433fc95c4c5cbc9
Martin Virtel <martin.virtel@gmail.com>
2019-06-13 17:36:32 +0200

    sozialhelden verlinkt

--- sozialhelden-projekte/app-files/data.js
+++ sozialhelden-projekte/app-files/data.js
@@ -2,7 +2,7 @@ var APP_DATA = {
   "scenes": [
     {
       "id": "0-581939753749445-2",
-      "name": "581939753.749445 2",
+      "name": "Sozialhelden Projekte",
       "levels": [
         {
           "tileSize": 256,
@@ -33,19 +33,19 @@ var APP_DATA = {
         {
           "yaw": -2.995623530271626,
           "pitch": 0.1253739996241947,
-          "title": "Sozialhelden e.V. (<a href=\"https://sozialhelden.de/\">https://sozialhelden.de/</a>)",
+          "title": "<a href=\"https://sozialhelden.de/\">Sozialhelden e.V.</a>",
           "text": "Die Sozialhelden sind ein Verein, der auf soziale Probleme aufmerksam machen möchte. Im Fokus ihrer Arbeit steht, die Arbeit für und mit Menschen mit Behinderungen. In dieser Grafik können Sie sich durch die verschiedenen Projekte der Sozialhelden klicken.&nbsp;"
         },
         {
           "yaw": 2.2854353183470426,
           "pitch": 0.2505783339113403,
-          "title": "Wheelmap(http://www.wheelmap.org)",
+          "title": "<a href=\"http://www.wheelmap.org\">Wheelmap</a>",
           "text": "Die Wheelmap ist eine interaktive Karte, auf der Nutzer sehen können, welche Orte barrierefrei zugängig sind, und zudem neue Orte eintragen können."
         },
         {
           "yaw": 0.6024138274492561,
           "pitch": -0.25053826577140725,
-          "title": "Travelable (<a href=\"https://travelable.info/\">https://travelable.info/</a>)",
+          "title": "<a href=\"https://travelable.info/\">Travelable</a>",
           "text": "Wie sieht es mit der Barrierefreiheit in anderen Städten aus? Diese Seite gibt einen Überblick für Menschen mit Behinderung, die auf Reisen gehen wollen."
         },
         {
@@ -57,25 +57,25 @@ var APP_DATA = {
         {
           "yaw": -2.5897337113109895,
           "pitch": -0.030167142765748167,
-          "title": "Ramp-Up.me (<a href=\"https://ramp-up.me/\">https://ramp-up.me/</a>)",
+          "title": "<a href=\"https://ramp-up.me/\">Ramp-Up.me</a>",
           "text": "Bei Ramp-Up handelt es sich um ein Angebot an Menschen, die barrierefreie Veranstaltungen planen wollen."
         },
         {
           "yaw": 1.3325136605047554,
           "pitch": -0.01264723946322377,
-          "title": "Gesellschaftsbilder (<a href=\"https://gesellschaftsbilder.de/\">https://gesellschaftsbilder.de/</a>)",
-          "text": "Eine Fotodatenbank vermeintlich ohne Klischees"
+          "title": "<a href=\"https://gesellschaftsbilder.de/\">Gesellschaftsbilder</a>",
+          "text": "Eine Fotodatenbank vermeintlich ohne Klischees."
         },
         {
           "yaw": 2.9266496799179826,
           "pitch": -0.22413450622567055,
-          "title": "TV für alle (<a href=\"https://tvfueralle.de/\">https://tvfueralle.de/</a>)",
+          "title": "<a href=\"https://tvfueralle.de/\">TV für alle</a>",
           "text": "Gibt einen Überblick über barrierefreie Fernsehsendungen. (Etwa solche, die mit Untertiteln oder mit Audiodeskription verfügbar sind.)"
         },
         {
           "yaw": -1.4174569967570818,
           "pitch": -0.10636541563900082,
-          "title": "Andersmacher (<a href=\"https://die-andersmacher.org/\">https://die-andersmacher.org/</a>&nbsp;)",
+          "title": "<a href=\"https://die-andersmacher.org/\">Andersmacher</a>",
           "text": "Dieses Projekt will Jugendlichen Vorbilder unter Menschen mit Behinderungen geben."
         }
       ]

8b469bb04f4313581a7f868b89abddae6008a577
Martin Virtel <martin.virtel@gmail.com>
2019-06-13 19:10:43 +0200

    mit videos

---
+++ 29/app-files/data.js
@@ -0,0 +1,61 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20190608_154615",
+      "name": "IMG_20190608_154615",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "yaw": -0.21491656517385138,
+        "pitch": -0.12990766954780497,
+        "fov": 1.3705483303097512
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.21642948718407595,
+          "pitch": -0.4720589924199903,
+          "title": "Willkommen&nbsp;",
+          "text": "<div class=\"ejs13-infobox\"><p>Willkommen\nin Oberschöneweide, in einem der einst größten\nIndustriegebiete \nBerlins. Hier arbeiteten zu\nDDR-Zeiten über 30.000 Menschen. Die\nTreuhandanstalt privatisierte hier fünf\nGroßbetriebe: Die Metallhütten und\nHalbzeugwerke,  das Institut\nfür Nachrichtentechnik,  das\n Transformatorenwerk, das\n Kabelwerk Oberspree und\ndas Werk\nfür Fernsehelektronik. \n</p>\n<p >\nWir\nsind hier auf dem Gelände der ehemaligen VEB Berliner Metallhütten-\nund Halbzeugwerke.  Das Industrieunternehmen wurde am 1. Januar\n1951 gegründet. Es war dem Ministerium für Schwerindustrie der DDR\nunterstellt.</p>\n<p>\nDas\nUnternehmen beschäftigte bis zu 2.300 Mitarbeiter, die im\nDreischichtbetrieb an Hoch- und Schmelzöfen arbeiteten. \n</p>\n<p>\n1990,\nnach\nder Wiedervereinigung, wurde der Betrieb eingestellt – der\nüberwiegende Teil des Fabrikgeländes ist\nbis heute ungenutzt.</p></div>"
+        },
+        {
+          "yaw": -2.9204113822176687,
+          "pitch": -0.2821936969008547,
+          "title": "Die Treuhand",
+          "text": "<iframe width=\"280px\" src=\"https://www.youtube.com/embed/5lEtbWfghcc\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+        },
+        {
+          "yaw": -1.6644270643889012,
+          "pitch": -0.14011290284871336,
+          "title": "Die Umbrüche in der ehemaligen DDR",
+          "text": "<iframe width=\"280px\" src=\"https://www.youtube.com/embed/gILkpOha0OU\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};

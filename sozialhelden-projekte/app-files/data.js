f4a162a17fb2c1390ee6fe368e7601dc27caeec0
Martin Virtel <martin.virtel@gmail.com>
2019-06-13 17:26:02 +0200

    +therese sozialhelden

---
+++ sozialhelden-projekte/app-files/data.js
@@ -0,0 +1,91 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-581939753749445-2",
+      "name": "581939753.749445 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 3.0552367842438946,
+        "pitch": 0.1514766534801133,
+        "fov": 1.2809123973965084
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.995623530271626,
+          "pitch": 0.1253739996241947,
+          "title": "Sozialhelden e.V. (<a href=\"https://sozialhelden.de/\">https://sozialhelden.de/</a>)",
+          "text": "Die Sozialhelden sind ein Verein, der auf soziale Probleme aufmerksam machen möchte. Im Fokus ihrer Arbeit steht, die Arbeit für und mit Menschen mit Behinderungen. In dieser Grafik können Sie sich durch die verschiedenen Projekte der Sozialhelden klicken.&nbsp;"
+        },
+        {
+          "yaw": 2.2854353183470426,
+          "pitch": 0.2505783339113403,
+          "title": "Wheelmap(http://www.wheelmap.org)",
+          "text": "Die Wheelmap ist eine interaktive Karte, auf der Nutzer sehen können, welche Orte barrierefrei zugängig sind, und zudem neue Orte eintragen können."
+        },
+        {
+          "yaw": 0.6024138274492561,
+          "pitch": -0.25053826577140725,
+          "title": "Travelable (<a href=\"https://travelable.info/\">https://travelable.info/</a>)",
+          "text": "Wie sieht es mit der Barrierefreiheit in anderen Städten aus? Diese Seite gibt einen Überblick für Menschen mit Behinderung, die auf Reisen gehen wollen."
+        },
+        {
+          "yaw": -0.3821949205922124,
+          "pitch": -0.04084977712413718,
+          "title": "JOBinklusive",
+          "text": "Eine Kampagne, die versucht mehr Menschen mit Behinderung in den regulären Arbeitsmarkt zu integrieren."
+        },
+        {
+          "yaw": -2.5897337113109895,
+          "pitch": -0.030167142765748167,
+          "title": "Ramp-Up.me (<a href=\"https://ramp-up.me/\">https://ramp-up.me/</a>)",
+          "text": "Bei Ramp-Up handelt es sich um ein Angebot an Menschen, die barrierefreie Veranstaltungen planen wollen."
+        },
+        {
+          "yaw": 1.3325136605047554,
+          "pitch": -0.01264723946322377,
+          "title": "Gesellschaftsbilder (<a href=\"https://gesellschaftsbilder.de/\">https://gesellschaftsbilder.de/</a>)",
+          "text": "Eine Fotodatenbank vermeintlich ohne Klischees"
+        },
+        {
+          "yaw": 2.9266496799179826,
+          "pitch": -0.22413450622567055,
+          "title": "TV für alle (<a href=\"https://tvfueralle.de/\">https://tvfueralle.de/</a>)",
+          "text": "Gibt einen Überblick über barrierefreie Fernsehsendungen. (Etwa solche, die mit Untertiteln oder mit Audiodeskription verfügbar sind.)"
+        },
+        {
+          "yaw": -1.4174569967570818,
+          "pitch": -0.10636541563900082,
+          "title": "Andersmacher (<a href=\"https://die-andersmacher.org/\">https://die-andersmacher.org/</a>&nbsp;)",
+          "text": "Dieses Projekt will Jugendlichen Vorbilder unter Menschen mit Behinderungen geben."
+        }
+      ]
+    }
+  ],
+  "name": "Sozialhelden Projekte",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
